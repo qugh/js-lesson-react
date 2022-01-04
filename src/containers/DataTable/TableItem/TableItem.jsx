88949b9d9ebb3144efffa929d043e55a9800c61c
Aleksandr <aleksandr.birkant@gmail.com>
2022-01-05 02:50:54 +0300

    delete unused folders

--- src/containers/DataTable/TableItem/TableItem.jsx
+++
@@ -1,9 +0,0 @@
-export const TableItem = (props) => {
-    return (
-        <tr>
-            <td>{props.position}</td>
-            <td>{props.location}</td>
-            <td>{props.price} y.e.</td>
-        </tr>
-    )
-}
